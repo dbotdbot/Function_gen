a47498d264a62255ef1798dfb5df3b1c64d234b6
Dylan Wheldale <dwheldale@gmail.com>
2019-06-01 19:22:38 +1000

    added AD9833 cpp class files

--- func_gen_proj/func_gen_proj.ino
+++ func_gen_proj/func_gen_proj.ino
@@ -14,6 +14,7 @@
  */
 #include <Wire.h> 
 #include <LiquidCrystal_I2C.h>
+#include "AD9833.h"
 
 LiquidCrystal_I2C lcd(0x27,16,2);                          // set the LCD address to 0x27 for a 16 chars and 2 line display
 
